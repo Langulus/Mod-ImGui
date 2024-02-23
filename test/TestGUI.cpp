b8c57398ccd7e8ffccc90764a466a526efa4c908
Dimo Markov <dimomarkov@gmail.com>
2024-02-24 01:26:34 +0200

    Improved test leak detection

--- test/TestGUI.cpp
+++ test/TestGUI.cpp
@@ -9,6 +9,7 @@
 #include <Entity/Thing.hpp>
 #include <catch2/catch.hpp>
 
+
 /// See https://github.com/catchorg/Catch2/blob/devel/docs/tostring.md        
 CATCH_TRANSLATE_EXCEPTION(::Langulus::Exception const& ex) {
    const Text serialized {ex};
@@ -16,21 +17,39 @@ CATCH_TRANSLATE_EXCEPTION(::Langulus::Exception const& ex) {
 }
 
 SCENARIO("GUI creation", "[gui]") {
-   Allocator::State memoryState;
+   static Allocator::State memoryState;
 
    for (int repeat = 0; repeat != 10; ++repeat) {
       GIVEN(std::string("Init and shutdown cycle #") + std::to_string(repeat)) {
          // Create root entity                                          
          Thing root;
          root.SetName("ROOT");
-
-         // Create runtime at the root                                  
          root.CreateRuntime();
-
-         // Load ImGui module                                           
          root.LoadMod("GLFW");
          root.LoadMod("Vulkan");
          root.LoadMod("ImGui");
+
+         WHEN("The GUI system is created via abstractions") {
+            auto window = root.CreateUnit<A::Window>(Traits::Size(640, 480));
+            auto renderer = root.CreateUnit<A::Renderer>();
+            auto gui = root.CreateUnit<A::UI::System>();
+
+            // Update once                                              
+            root.Update(Time::zero());
+            root.DumpHierarchy();
+
+            REQUIRE(window.GetCount() == 1);
+            REQUIRE(window.CastsTo<A::Window>(1));
+            REQUIRE(window.IsSparse());
+
+            REQUIRE(renderer.GetCount() == 1);
+            REQUIRE(renderer.CastsTo<A::Renderer>(1));
+            REQUIRE(renderer.IsSparse());
+
+            REQUIRE(gui.GetCount() == 1);
+            REQUIRE(gui.CastsTo<A::UI::System>(1));
+            REQUIRE(gui.IsSparse());
+         }
 
       #if LANGULUS_FEATURE(MANAGED_REFLECTION)
          WHEN("The GUI system is created via tokens") {
@@ -40,49 +59,21 @@ SCENARIO("GUI creation", "[gui]") {
 
             // Update once                                              
             root.Update(Time::zero());
+            root.DumpHierarchy();
 
-            THEN("Various traits change") {
-               root.DumpHierarchy();
+            REQUIRE(window.GetCount() == 1);
+            REQUIRE(window.CastsTo<A::Window>(1));
+            REQUIRE(window.IsSparse());
 
-               REQUIRE(window.GetCount() == 1);
-               REQUIRE(window.CastsTo<A::Window>(1));
-               REQUIRE(window.IsSparse());
+            REQUIRE(renderer.GetCount() == 1);
+            REQUIRE(renderer.CastsTo<A::Renderer>(1));
+            REQUIRE(renderer.IsSparse());
 
-               REQUIRE(renderer.GetCount() == 1);
-               REQUIRE(renderer.CastsTo<A::Renderer>(1));
-               REQUIRE(renderer.IsSparse());
-
-               REQUIRE(gui.GetCount() == 1);
-               REQUIRE(gui.CastsTo<A::UI::System>(1));
-               REQUIRE(gui.IsSparse());
-            }
+            REQUIRE(gui.GetCount() == 1);
+            REQUIRE(gui.CastsTo<A::UI::System>(1));
+            REQUIRE(gui.IsSparse());
          }
       #endif
-
-         WHEN("The GUI system is created via abstractions") {
-            auto window = root.CreateUnit<A::Window>(Traits::Size(640, 480));
-            auto renderer = root.CreateUnit<A::Renderer>();
-            auto gui = root.CreateUnit<A::UI::System>();
-
-            // Update once                                              
-            root.Update(Time::zero());
-
-            THEN("Various traits change") {
-               root.DumpHierarchy();
-
-               REQUIRE(window.GetCount() == 1);
-               REQUIRE(window.CastsTo<A::Window>(1));
-               REQUIRE(window.IsSparse());
-
-               REQUIRE(renderer.GetCount() == 1);
-               REQUIRE(renderer.CastsTo<A::Renderer>(1));
-               REQUIRE(renderer.IsSparse());
-
-               REQUIRE(gui.GetCount() == 1);
-               REQUIRE(gui.CastsTo<A::UI::System>(1));
-               REQUIRE(gui.IsSparse());
-            }
-         }
 
          // Check for memory leaks after each cycle                     
          REQUIRE(memoryState.Assert());
